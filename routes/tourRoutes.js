de95afda21e5b23863d2c88e8f240794b4bd414c
misharexviashvili <misharexviashvili@yahoo.com>
2023-05-30 00:02:35 +0400

    virtual review population implementation 1583

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,31 +1,39 @@
-const express = require('express');
-const tourController = require('./../controllers/tourController');
-const authController = require('./../controllers/authController');
-
+const express = require("express");
+const tourController = require("./../controllers/tourController");
+const authController = require("./../controllers/authController");
+const reviewController = require("./../controllers/reviewController");
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
 router
-  .route('/top-5-cheap')
+  .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router.route("/tour-stats").get(tourController.getTourStats);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 
 router
-  .route('/')
+  .route("/")
   .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(tourController.getTour)
   .patch(tourController.updateTour)
   .delete(
     authController.protect,
-    authController.restrictTo('admin', 'lead-guide'),
+    authController.restrictTo("admin", "lead-guide"),
     tourController.deleteTour
+  );
+
+router
+  .route("/:tourId/reviews")
+  .post(
+    authController.protect,
+    authController.restrictTo("user"),
+    reviewController.createReview
   );
 
 module.exports = router;
