ae86921997ff34e64ff83af74fa3ea1f13b6c020
misharexviashvili <misharexviashvili@yahoo.com>
2023-06-01 23:36:47 +0400

    review router mounting 159

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const tourController = require("./../controllers/tourController");
 const authController = require("./../controllers/authController");
-const reviewController = require("./../controllers/reviewController");
 const router = express.Router();
-
+const reviewRouter = require("./reviewRoutes");
 // router.param('id', tourController.checkID);
 
+router.use('/:tourId/reviews', reviewRouter);
 router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -26,14 +26,6 @@ router
     authController.protect,
     authController.restrictTo("admin", "lead-guide"),
     tourController.deleteTour
-  );
-
-router
-  .route("/:tourId/reviews")
-  .post(
-    authController.protect,
-    authController.restrictTo("user"),
-    reviewController.createReview
   );
 
 module.exports = router;
